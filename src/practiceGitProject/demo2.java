508cecee6e0db46be69dcecaa251433db98fc067
Smrutimudbikar240394 <smrutimudbikar240394@gmail.com>
2024-12-02 09:32:07 +0530

    Update demo2.java

--- src/practiceGitProject/demo2.java
+++ src/practiceGitProject/demo2.java
@@ -1,5 +1,7 @@
+package practiceGitProject;
+
 public class demo2{
-  public static void main(String args[])
+  public static void main(String[] args)
   {
     System.out.println("demo 2 running");
   }
